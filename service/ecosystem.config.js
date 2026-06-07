e45e786da797238a79602a634ff987d4fc7dcc7e
Kirin Anthia <59920578+NatureGecko@users.noreply.github.com>
2026-06-07 23:36:52 +0700

    Update 2026-06-07

--- service/ecosystem.config.js
+++ service/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'tacra-social-hub',
-      script: 'dist/main.js',
+      script: 'dist/src/main.js',
       env: {
         NODE_ENV: 'production',
       },
