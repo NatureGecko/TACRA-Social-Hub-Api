4c95e84622c58fb0bf16bb861a425f0e16c1a5fe
Kirin Anthia <59920578+NatureGecko@users.noreply.github.com>
2026-06-07 18:29:27 +0700

    Update 2026-06-07

--- service/src/service/profile/profile.service.ts
+++ service/src/service/profile/profile.service.ts
@@ -1,13 +1,15 @@
 import { Storage } from '@google-cloud/storage';
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../../modules/prisma/prisma.service';
 
 @Injectable()
 export class ProfileService {
   private readonly storage: Storage;
+  private readonly profileFilePath: string = 'user_profile_image';
   private readonly bucketName: string =
     process.env.GOOGLE_CLOUD_STORAGE_BUCKET_NAME || '';
 
-  constructor() {
+  constructor(private readonly prisma: PrismaService) {
     this.storage = new Storage({
       projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
       credentials: {
@@ -30,4 +32,18 @@ export class ProfileService {
     const [contents] = await file.download();
     return contents;
   }
+
+  // [ GET ] profile/gallery/:userId
+  async galleryByUserId(userId: string) {
+    // const taregtMedia = await this.prisma.userMedia.findMany({
+    //   where: { userProfileId: userId },
+    // });
+    const context = this.storage.bucket(this.bucketName).file(``);
+  }
+
+  // [ GET ] profile/profile-image/:userId
+  async profileImageByUserId(userId: string) {}
+
+  // [ GET ] profile/profile-image-list/:userId
+  async profileImageListByUserId(userId: string) {}
 }
